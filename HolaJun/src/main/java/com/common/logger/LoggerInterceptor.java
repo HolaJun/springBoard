5964ab35afcd15099d0e405db623f2e8ffa8fd2a
HolaJun <ljh_0107@naver.com>
2018-12-23 15:43:12 +0900

    update

--- HolaJun/src/main/java/com/common/logger/LoggerInterceptor.java
+++ HolaJun/src/main/java/com/common/logger/LoggerInterceptor.java
@@ -3,14 +3,16 @@ package com.common.logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 
 public class LoggerInterceptor extends HandlerInterceptorAdapter {
-    protected Log log = LoggerFactory.getLog(LoggerInterceptor.class);
-     
+    protected Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (log.isDebugEnabled()) {
--- HolaJun/src/main/java/com/sample/controller/SampleController.java
+++ HolaJun/src/main/java/com/sample/controller/SampleController.java
@@ -3,13 +3,14 @@ package com.sample.controller;
 import java.util.Map;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SampleController {
-    Logger log = Logger.getLogger(this.getClass());
+	Logger log = LoggerFactory.getLogger(this.getClass());
      
     @RequestMapping(value="/sample/openSampleList.do")
     public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
