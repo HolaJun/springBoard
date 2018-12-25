b8c507ed5635fd8413d411e57a383ba8a3739aa8
HolaJun <ljh_0107@naver.com>
2018-12-26 01:45:35 +0900

    update

--- HolaJun/src/main/java/com/sample/controller/SampleController.java
+++ HolaJun/src/main/java/com/sample/controller/SampleController.java
@@ -1,17 +1,24 @@
 package com.sample.controller;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.sample.service.SampleService;
 
 @Controller
 public class SampleController {
 	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Resource(name="sampleService")
+    private SampleService sampleService;
     
 	@RequestMapping(value="/")
 	public ModelAndView home() throws Exception {
@@ -23,11 +30,14 @@ public class SampleController {
 		return mv;
 	}
 	
-    @RequestMapping(value="/sample/openSampleList.do")
-    public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
-        ModelAndView mv = new ModelAndView("");
-        log.debug("인터셉터 테스트");
+	@RequestMapping(value="/sample/openSampleBoardList.do")
+    public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("/sample/boardList");
+         
+        List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+        mv.addObject("list", list);
          
         return mv;
     }
+
 }
